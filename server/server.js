b20f492d0c2be7e7b254d35f0544d4542c74bf94
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-05-16 14:24:23 +0600

    .

--- server/server.js
+++ server/server.js
@@ -1,14 +1,50 @@
+const path = require('path');
 const http = require('http');
+const compression = require('compression');
 const express = require('express');
-const app = express();
-const server = http.createServer(app);
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const socketIO = require('socket.io');
+const uuid = require("uuid");
 
-app.get('/', (req, res) => {
-    res.sendFile('/index.html');
+const version = process.env.npm_package_version;
+
+const apiRequestLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minute
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests. Temporarily blocked from PokeTab server. Please try again later",
+    standardHeaders: false, // Return rate limit info in the `RateLimit-*` headers
+      legacyHeaders: false // Disable the `X-RateLimit-*` headers
 });
 
-console.log('Server running at http://localhost:3000/');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+app.disable('x-powered-by');
+
+//view engine setup
+app.set('views', path.join(__dirname, '../public/views'));
+app.set('view engine', 'ejs');
+app.set('trust proxy', 1);
+
+app.use(cors());
+app.use(compression());
+app.use(express.static(publicPath));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+
+app.use(apiRequestLimiter);
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
