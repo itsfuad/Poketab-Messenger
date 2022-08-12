cf1dd2981e9cf7e8f65d981909518969b17756aa
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-08-12 20:11:05 +0600

    .

--- server/server.js
+++ server/server.js
@@ -59,15 +59,15 @@ app.set('views', path.join(__dirname, '../public/views'));
 app.set('view engine', 'ejs');
 app.set('trust proxy', 1);
 
-app.use(cors());
-app.use(compression());
-app.use(express.static(publicPath));
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
-
-app.use(apiRequestLimiter);
+app.use(cors(),
+  compression(),
+  express.static(publicPath),
+  express.json(),
+  express.urlencoded({
+    extended: false
+  }),
+  apiRequestLimiter
+);
 
 app.get('/', (_, res) => {
     res.redirect('/join');
