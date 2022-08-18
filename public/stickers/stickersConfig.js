dd1e27b6b63871762ff8db36c8bf0b33652023ec
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-08-18 22:10:13 +0600

    more stickers

--- public/stickers/stickersConfig.js
+++ public/stickers/stickersConfig.js
@@ -1,7 +1,10 @@
 const Stickers = [
+    {"name": "robo", "count": "16", "icon": "14"},
     {"name": "cat", "count": "29", "icon": "15"},
+    {"name": "emojis", "count": "26", "icon": "9"},
     {"name": "cutecat", "count": "33", "icon": "30"},
     {"name": "skully", "count": "26", "icon": "23"},
+    {"name": "monkey", "count": "24", "icon": "4"},
     {"name": "frog", "count": "30", "icon": "18"},
 ]
 
