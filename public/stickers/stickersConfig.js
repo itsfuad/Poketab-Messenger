442ded73ba97abd4d61f412b5619ccceac4ef913
Fuad Hasan <49100463+itsfuad@users.noreply.github.com>
2022-11-24 23:23:29 +0600

    Stickers multiple instances

--- public/stickers/stickersConfig.js
+++ public/stickers/stickersConfig.js
@@ -7,7 +7,7 @@ const Stickers = [
     {"name": "robo", "count": "16", "icon": "14"},
     {"name": "crab", "count": "24", "icon": "1"},
     {"name": "cutecat", "count": "33", "icon": "30"},
-    {"name": "skully", "count": "26", "icon": "23"},
+    {"name": "skully", "count": "25", "icon": "23"},
     {"name": "monkey", "count": "24", "icon": "4"},
     {"name": "frog", "count": "30", "icon": "18"},
 ]
